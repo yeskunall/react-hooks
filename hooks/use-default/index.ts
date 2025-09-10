0ae07d3f021aef88f9b483282114219496e97361
Kunall Banerjee <hey@kimchiii.space>
2025-09-10 02:03:15 -0400

    Add missing TSDocs

--- hooks/use-default/index.ts
+++ hooks/use-default/index.ts
@@ -1,5 +1,28 @@
 import { useState } from "react"
 
+/**
+ * Manage state but automatically falls back to a default value the state is `null` or `undefined`.
+ *
+ * @typeParam T - Type of the state value.
+ *
+ * @param defaultValue - Default value to use when state is `null` or `undefined`.
+ * @param initial - Initial state.
+ * @returns A tuple containing the resolved `value` and a setter function to update the state.
+ *
+ * @example
+ * ```tsx
+ * function Profile({ user }: { user?: string | null }) {
+ *   const { value: name, set: setName } = useDefault("Anonymous", user);
+ *
+ *   return (
+ *     <div>
+ *       <p>Hello, {name}!</p>
+ *       <button onClick={() => setName(null)}>Reset</button>
+ *     </div>
+ *   );
+ * }
+ * ```
+ */
 export function useDefault<T>(defaultValue: T, initialValue?: T | (() => T) | null | undefined) {
   const [value, setValue] = useState<T | null | undefined>(initialValue ?? defaultValue)
 
